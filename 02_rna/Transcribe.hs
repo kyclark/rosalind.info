03b9c69b212f1e491139d2997f4c5f9c8c6a2ed1
Ken Youens-Clark <kyclark@gmail.com>
2020-06-05 14:07:43 -0700

    reorg

--- 02_rna/Transcribe.hs
+++
@@ -1,8 +0,0 @@
-import System.Environment
-
-main = do
-    [f] <- getArgs
-    putStrLn f
-    c <- readFile f
-    putStrLn c
-    --putStrLn (lines $ read c)
--- 02_rna/transcribe.pl6
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env perl6
-
-sub MAIN (Str $string) {
-    my $dna = $string.IO.e ?? $string.IO.slurp !! $string;
-    say $dna.subst('T', 'U');
-}
