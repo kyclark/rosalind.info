96655cf78c635a8eb6b084f5d81aba99b5c7ae97
Ken Youens-Clark <kyclark@gmail.com>
2020-07-22 15:37:30 -0700

    some ideas but not working yet

--- 22_lgis/lgis.py
+++ 22_lgis/lgis.py
@@ -3,7 +3,7 @@
 
 import argparse
 import operator
-from itertools import combinations, starmap
+from itertools import combinations, starmap, takewhile
 from typing import NamedTuple, List, Any, Tuple, Callable, Optional
 
 
@@ -19,11 +19,13 @@ def get_args() -> Args:
         description='Longest Increasing Subsequence',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('pi',
-                        metavar='int',
-                        type=int,
-                        nargs='+',
-                        help='Sequence of integers')
+    parser.add_argument(
+        'pi',
+        metavar='int',
+        # type=int,
+        type=str,
+        nargs='+',
+        help='Sequence of integers')
 
     args = parser.parse_args()
 
@@ -35,26 +37,57 @@ def main() -> None:
     """Make a jazz noise here"""
 
     args = get_args()
-    num_pi = len(args.pi)
-    deciles = [num_pi] if num_pi < 10 else decimate(args.pi)
+    flip = lambda t: tuple(reversed(t))
+    print(args.pi)
+    pi = sorted(list(map(flip, enumerate(args.pi))))
+    rev = list(reversed(sorted(list(map(flip, enumerate(args.pi))))))
+    print(pi)
+    print(rev)
 
-    asc, desc = (), ()
-    low = 0
-    for high in deciles:
-        print(low, high)
-        if asc_ := lgis(args.pi, is_asc, low, high):
-            asc = asc_
-        if desc_ := lgis(args.pi, is_desc, low, high):
-            desc = desc_
+    pos = list(map(snd, pi))
+    show = lambda xs: ' '.join(map(lambda i: str(args.pi[i]), xs))
 
-        low = high
+    print('asc pos', pos)
+    print(show(longest(stretch(pos, operator.lt))))
 
-        if asc and desc:
-            break
+#    pos = list(reversed(pos))
+#    print('des pos', pos)
+#    print(show(longest(stretch(pos, operator.gt))))
 
-    print(' '.join(map(str, asc)))
-    print(' '.join(map(str, desc)))
+    # for op in [operator.lt, operator.gt]:
+    #     vals = longest(stretch(list(map(snd, pi)), op))
+    #     print(op, vals)
+    #     # print(' '.join(map(str, map(lambda i: fst(args.pi[i]), vals))))
+    #     print(' '.join(map(lambda i: str(args.pi[i]), vals)))
 
+    # Ascending
+    # print(list(map(fst, longest(pi))))
+    # print(' '.join(longest(pi)))
+
+    # pi = list(reversed(pi))
+    # print(' '.join(longest(pi)))
+
+    # print(list(map(fst, longest(pi))))
+
+    # num_pi = len(args.pi)
+    # deciles = [num_pi] if num_pi < 10 else decimate(args.pi)
+
+    # asc, desc = (), ()
+    # low = 0
+    # for high in deciles:
+    #     print(low, high)
+    #     if asc_ := lgis(args.pi, is_asc, low, high):
+    #         asc = asc_
+    #     if desc_ := lgis(args.pi, is_desc, low, high):
+    #         desc = desc_
+
+    #     low = high
+
+    #     if asc and desc:
+    #         break
+
+    # print(' '.join(map(str, asc)))
+    # print(' '.join(map(str, desc)))
 
     # for k in deciles:
     #     for i, combo in enumerate(combinations(args.pi, k)):
@@ -63,8 +96,6 @@ def main() -> None:
     #         if is_asc(combo) and len(combo) > len(asc):
     #             print('Found asc!')
     #             print(combo)
-
-
 
     # len_pi = len(args.pi)
     # asc, desc = (), ()
@@ -122,9 +153,9 @@ def main() -> None:
 
 
 # --------------------------------------------------
-def lgis(xs: List[Any], f: Callable, low: int, high: int) -> Optional[List[Any]]:
+def lgis(xs: List[Any], f: Callable, low: int,
+         high: int) -> Optional[List[Any]]:
     """Find LGIS up to max_ using function"""
-
     def search(k: int):
         """Return first combo that matches"""
         for i, combo in enumerate(combinations(xs, k)):
@@ -200,6 +231,110 @@ def decimate(xs: List[Any]) -> Optional[List[int]]:
 
     if size := len(xs) // 100:
         return [size * i for i in range(1, 11)]
+
+
+# --------------------------------------------------
+def chain(xs: List[List[Any]]) -> List[Any]:
+    """My version of chain to flatten lists of unique values"""
+
+    ret = []
+
+    for x in xs:
+        for v in x:
+            if v not in ret:
+                ret.append(v)
+
+    return ret
+
+
+# --------------------------------------------------
+def test_chain() -> None:
+    """Test chain"""
+
+    assert chain([]) == []
+    assert chain([[1, 2]]) == [1, 2]
+    assert chain([[1, 2], [2, 3]]) == [1, 2, 3]
+
+
+# --------------------------------------------------
+def stretch(xs: List[int], f: Callable) -> List[List[int]]:
+    """Find stretches of ints"""
+
+    print('xs', xs)
+    ret = []
+    last = []
+
+    for i in range(len(xs) - 1):
+        x, y = xs[i], xs[i + 1]
+        if f(x, y):
+            last.append([x, y])
+        elif last:
+            ret.append(chain(last))
+            last = []
+
+    if last:
+        ret.append(chain(last))
+
+    print('ret', ret)
+    return ret
+
+
+# --------------------------------------------------
+def test_stretch() -> None:
+    """Test stretch"""
+
+    assert stretch([], operator.lt) == []
+    assert stretch([1], operator.lt) == []
+    assert stretch([1, 2], operator.lt) == [[1, 2]]
+    assert stretch([1, 2, 3], operator.lt) == [[1, 2, 3]]
+    assert stretch([1, 3, 2], operator.gt) == [[3, 2]]
+    assert stretch([2, 3, 1, 0, 4, 5], operator.lt) == [[2, 3], [0, 4, 5]]
+    assert stretch([2, 3, 1, 0, 4, 5], operator.gt) == [[3, 1, 0]]
+
+
+# --------------------------------------------------
+def longest(xs: List[List[Any]]) -> List[Any]:
+    """Return the longest list"""
+
+    return sorted(xs, key=len)[-1] if xs else []
+
+
+# --------------------------------------------------
+def test_longest() -> None:
+    """Test longest"""
+
+    assert longest([]) == []
+    assert longest([[1]]) == [1]
+    assert longest([[2, 3], [1]]) == [2, 3]
+    assert longest([[1], [4, 5, 6], [2, 3]]) == [4, 5, 6]
+
+
+# --------------------------------------------------
+def fst(tup: Tuple[Any, Any]) -> Any:
+    """Return second of tuple"""
+
+    return tup[0]
+
+
+# --------------------------------------------------
+def test_fst() -> None:
+    """Test fst"""
+
+    assert fst(('foo', 'bar')) == 'foo'
+
+
+# --------------------------------------------------
+def snd(tup: Tuple[Any, Any]) -> Any:
+    """Return second of tuple"""
+
+    return tup[1]
+
+
+# --------------------------------------------------
+def test_snd() -> None:
+    """Test snd"""
+
+    assert snd(('foo', 'bar')) == 'bar'
 
 
 # --------------------------------------------------
